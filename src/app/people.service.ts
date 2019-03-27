ee84b41412a8efdcd1c605b49e6ed5561a049970
fbath <phoenixa@gmx.de>
2019-03-27 19:15:21 +0100

    created api service call

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
+  private endpoint = "https://swapi.co/api/people/?page=";
+  private httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json"
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
+
+  getPeople(page): Observable<any> {
+    return this.http.get(this.endpoint + page).pipe(map(this.extractData));
+  }
+
 }
